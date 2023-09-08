b42f9994df18606ef5d935ab7b6d368b86e89615
Austin-Han <austinkyuhan@gmail.com>
2023-09-07 19:26:01 -0700

    First commit

---
+++ src/utilities.js
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+export const drawRect = (detections, ctx) => {
+    detections.forEach(prediction=>{
+        const [x,y,width,height] = prediction['bbox'];
+        const text = prediction['class'];   
+
+
+        const color = '#' + Math.floor(Math.random()*16777215).toString(16);
+        ctx.strokeStyle = color
+        ctx.Font = '18px Arial'
+        ctx.fillStyle = color
+
+        ctx.beginPath()
+        ctx.fillText(text, x, y)
+        ctx.rect(x, y, width, height)
+        ctx.stroke()
+    })
+}
